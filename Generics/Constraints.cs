aa87fe42213569f6e4feece182862f4181941b01
ppedvLukasK <lukask@ppedv.de>
2023-09-25 16:18:22 +0200

    M004

---
+++ Generics/Constraints.cs
@@ -0,0 +1,32 @@
+﻿namespace Generics;
+
+public class Constraints
+{
+
+}
+
+public class DataStore1<T> where T : class { }
+
+public class DataStore2<T> where T : struct { }
+
+public class DataStore3<T> where T : Constraints { } //Die Klasse selbst, oder eine Unterklasse
+
+public class DataStore4<T> where T : IEnumerable<T> { }
+
+public class DataStore5<T> where T : new() { } //new(): T muss einen Standardkonstruktor haben
+
+public class DataStore6<T> where T : Enum { } //Enum: Ein Enum (kein Enumwert)
+
+public class DataStore7<T> where T : Delegate { } //Delegate: Ein Delegate (Action, Func, EventHandler, ...)
+
+public class DataStore8<T> where T : unmanaged { }
+//https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+public class DataStore9<T> where T : class, Delegate, new() { } //Mehrere Einschränkungen auf ein Constraint
+
+public class DataStore10<T1, T2>
+	where T1 : class, new()
+	where T2 : struct
+{
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Generics;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		List<int> x = new();
+		x.Add(1); //T ist jetzt int
+
+		DataStore<int> ds;
+	}
+}
+
+public class DataStore<T> : IProgress<T>
+{
+	public T[] data;
+
+	public List<T> List => data.ToList();
+
+	public T GetIndex(int index)
+	{
+		if (index < 0 || index > data.Length)
+			return default; //default: Nimmt den Standardwert von T
+		return data[index];
+	}
+
+	public void Add(T item, int index)
+	{
+		data[index] = item; 
+	}
+
+	public void Report(T value)
+	{
+        //...
+    }
+
+	public void Methode<TT>()
+	{
+        Console.WriteLine(default(TT));
+        Console.WriteLine(typeof(TT)); //Konkreter Typ hinter TT
+        Console.WriteLine(nameof(TT)); //Name des Typs als String (hinter TT)
+
+		object o = 123;
+		Convert.ChangeType(o, typeof(TT)); //Cast mit Generic
+    }
+}
+
+public class Person { }
+
+public class Mensch : Person { }
